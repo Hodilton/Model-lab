4eb28dbc70522cba5942db78bff0fb20873b3913
Ilya Barsukov <amenohitottsunokami@yandex.ru>
2024-09-20 19:30:51 +0700

    Prac_1

---
+++ Prac_1/Prac_1.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+
+#include "./file work/file_processor.h"
+#include "./Vectors.h"
+
+int main() {
+	auto vecs = file_work::File<Vectors<int>>::read(
+		file_work::PathParams("./data", "vec_params", "json"));
+
+	vecs.fill();
+
+	auto sums = vecs.sum_each_vector();
+	auto min = *(std::min_element(sums.begin(), sums.end()));
+
+	cout << vecs << min;
+
+	return 0;
+}
